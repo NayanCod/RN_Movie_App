746c4580403c5e55c3d3ed8c4720dbea8dc5325f
NayanCod <kumarnayab6@gmail.com>
2025-03-11 12:47:33 +0530

    project setup

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 justify-center items-center bg-primary">
+      <Text className="text-5xl text-light-100 font-bold">hello world !</Text>
+    </View>
+  );
+}
