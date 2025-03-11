11786daaaa5f0944117bf97e07ef0078f91fc848
NayanCod <kumarnayab6@gmail.com>
2025-03-11 12:59:48 +0530

    applied taiwlind classes for test - successfull

--- app/index.tsx
+++ app/index.tsx
@@ -2,7 +2,7 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View className="flex-1 justify-center items-center bg-primary">
+    <View className="flex-1 justify-center items-center">
       <Text className="text-5xl text-light-100 font-bold">hello world !</Text>
     </View>
   );
