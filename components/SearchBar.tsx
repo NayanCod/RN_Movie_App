61a04cbc2299dac4c3067e59b3039d328e64dbaa
NayanCod <kumarnayab6@gmail.com>
2025-03-12 14:28:12 +0530

    creating the search page

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -5,18 +5,20 @@ import { icons } from '@/constants/icons'
 
 interface Props{
     placeholder: string,
+    value?: string,
+    onChangeText?: (text: string) => void;
     onPress?: () => void;
 }
 
-const SearchBar = ({placeholder, onPress}: Props) => {
+const SearchBar = ({placeholder, onPress, value, onChangeText}: Props) => {
   return (
     <View className='flex-row items-center bg-dark-100 px-5 py-1 rounded-full'>
       <Image source={icons.search} className='size-5' resizeMode='contain' tintColor="#ab8bff" />
       <TextInput 
       onPress={onPress}
       placeholder={placeholder}
-      value=''
-      onChangeText={() => {}}
+      value={value}
+      onChangeText={onChangeText}
       placeholderTextColor="#a8b5db"
       className='flex-1 ml-2 text-white'
       />
