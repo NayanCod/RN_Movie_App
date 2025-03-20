f38b7d36450e87eeba4635a91ff5a6fbe5edca65
NayanCod <kumarnayab6@gmail.com>
2025-03-20 10:42:16 +0530

    ui chanegs in the home page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,7 +1,15 @@
 import SearchBar from "@/components/SearchBar";
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
-import { ActivityIndicator, FlatList, Image, RefreshControl, Text, TouchableOpacity, View } from "react-native";
+import {
+  ActivityIndicator,
+  FlatList,
+  Image,
+  RefreshControl,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import { useRouter } from "expo-router";
 import useFetch from "@/services/useFetch";
 import { fetchMovies } from "@/services/api";
@@ -21,13 +29,14 @@ export default function Index() {
     loading,
     error,
     refetch: refetchMovies,
-  } = useFetch(() => fetchMovies({ query: "", page: page}));
+  } = useFetch(() => fetchMovies({ query: "", page: page }));
 
   useEffect(() => {
     if (fetchedMovies) {
       setMovies((prevMovies) => {
         const filteredMovies = fetchedMovies.filter(
-          (movie: Movie) => !prevMovies.some((prevMovie) => prevMovie?.id === movie?.id)
+          (movie: Movie) =>
+            !prevMovies.some((prevMovie) => prevMovie?.id === movie?.id)
         );
         return page === 1 ? filteredMovies : [...prevMovies, ...filteredMovies];
       });
@@ -49,7 +58,10 @@ export default function Index() {
     refetchMovies();
   }, [page]);
 
-  const renderMovieItem = useCallback(({ item }: {item: Movie}) => <MovieCard {...item} />, []);
+  const renderMovieItem = useCallback(
+    ({ item }: { item: Movie }) => <MovieCard {...item} />,
+    []
+  );
 
   const handleScroll = (event: any) => {
     const offsetY = event.nativeEvent.contentOffset.y;
@@ -63,79 +75,87 @@ export default function Index() {
   return (
     <View className="flex-1 bg-primary">
       <Image source={images.bg} className="absolute w-full z-0" />
-        {loading && page == 1 ? (
-          <ActivityIndicator
-          size='large'
+      {loading && page == 1 ? (
+        <ActivityIndicator
+          size="large"
           color="#0000ff"
-          className="mt-10 self-center" 
-          />
-        ): error ? (
-          <Text>Error: {error?.message}</Text>
-        ) : (
-          <>
-          <FlatList
-          ref={flatListRef}
-          data={movies}
-          // renderItem={({ item }) => <MovieCard {...item} />}
-          renderItem={renderMovieItem}
-          keyExtractor={(item) => item.id.toString()}
-          numColumns={3}
-          columnWrapperStyle={{
-            justifyContent: 'flex-start',
-            gap: 20,
-            paddingRight: 5,
-            marginBottom: 10
-          }}
-          className="mt-2 pb-32 px-5"
-          refreshControl={
-            <RefreshControl
-              refreshing={refreshing}
-              onRefresh={onRefresh}
-              colors={["#ff0000", "#00ff00", "#0000ff"]}
-              tintColor="#ffffff"
-              title="Pull to refresh"
-              titleColor="#ffffff"
-            />
-          }
-          onEndReached={loadMoreMovies}
-          onEndReachedThreshold={0.8}
-          initialNumToRender={12}
-          maxToRenderPerBatch={12}
-          windowSize={5}
-          removeClippedSubviews={true}
-          onScroll={handleScroll}
-          scrollEventThrottle={16}
-          ListHeaderComponent={
-            <>
-            <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
-              <SearchBar
-                onPress={() => router.push("/search")}
-                placeholder="Search movies..."
-              />
-              <Text className="text-lg text-white font-bold mt-5 mb-3">
-                Latest Movies
-              </Text>
-            </>
-          }
-          ListFooterComponent={
-            loading && page > 1 ? (
-              <View className="h-32">
-              <ActivityIndicator size="large" color="#0000ff" />
-              </View>
-            ) : null
-          }
+          className="mt-10 self-center"
         />
-        {showScrollTop && (
+      ) : error ? (
+        <Text>Error: {error?.message}</Text>
+      ) : (
+        <>
+          <FlatList
+            ref={flatListRef}
+            data={movies}
+            // renderItem={({ item }) => <MovieCard {...item} />}
+            renderItem={renderMovieItem}
+            keyExtractor={(item) => item.id.toString()}
+            numColumns={3}
+            columnWrapperStyle={{
+              justifyContent: "flex-start",
+              gap: 20,
+              paddingRight: 5,
+              marginBottom: 10,
+            }}
+            className="mt-2 pb-32 px-5"
+            refreshControl={
+              <RefreshControl
+                refreshing={refreshing}
+                onRefresh={onRefresh}
+                colors={["#ff0000", "#00ff00", "#0000ff"]}
+                tintColor="#ffffff"
+                title="Pull to refresh"
+                titleColor="#ffffff"
+              />
+            }
+            onEndReached={loadMoreMovies}
+            onEndReachedThreshold={0.8}
+            initialNumToRender={12}
+            maxToRenderPerBatch={12}
+            windowSize={5}
+            removeClippedSubviews={true}
+            onScroll={handleScroll}
+            scrollEventThrottle={16}
+            ListHeaderComponent={
+              <>
+                <View className="w-full flex-row justify-center mt-20 mb-5 items-center">
+                  <Image source={icons.logo} className="w-12 h-10" />
+                </View>
+                <View className="my-5">
+                <SearchBar
+                  onPress={() => router.push("/search")}
+                  placeholder="Search movies..."
+                />
+                </View>
+                <Text className="text-lg text-white font-bold mt-5 mb-3">
+                  Latest Movies
+                </Text>
+              </>
+            }
+            ListFooterComponent={
+              loading && page > 1 ? (
+                <View className="h-32">
+                  <ActivityIndicator size="large" color="#0000ff" />
+                </View>
+              ) : null
+            }
+          />
+          {showScrollTop && (
             <TouchableOpacity
               onPress={scrollToTop}
               className="absolute bottom-28 right-8 bg-accent p-3 rounded-full elevation-sm"
             >
               {/* <AntDesign name="arrowup" size={24} color="#fff" /> */}
-              <Image source={icons.up} className="w-6 h-6" tintColor="#ffffff"/>
+              <Image
+                source={icons.up}
+                className="w-6 h-6"
+                tintColor="#ffffff"
+              />
             </TouchableOpacity>
           )}
         </>
-        )}
+      )}
     </View>
   );
 }
